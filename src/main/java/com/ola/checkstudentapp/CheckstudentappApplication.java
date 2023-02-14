544d486449fbae6b62d14f59e2cd750f38562f64
Ola00 <aleksandra.beczkowska00@gmail.com>
2023-02-14 09:06:09 +0100

    Basic

---
+++ src/main/java/com/ola/checkstudentapp/CheckstudentappApplication.java
@@ -0,0 +1,13 @@
+package com.ola.checkstudentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstudentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckstudentappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ola/checkstudentapp/CheckstudentappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ola.checkstudentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstudentappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
