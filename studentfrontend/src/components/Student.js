41caa5e0bfbb3ddbf3ef0c93d30968ed46cf86c5
Ola00 <aleksandra.beczkowska00@gmail.com>
2023-02-18 17:48:52 +0100

    Fronetend ok

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -1,31 +1,44 @@
-import * as React from 'react';
+// import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container } from '@mui/system';
 import { Paper } from '@mui/material';
 
+// import { makeStyles } from '@material-ui/core/styles';
 
-
-
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       '& > *': {
+//         margin: theme.spacing(1),
+       
+//       },
+//     },
+//   }));
 export default function Student() {
-    const paperStyle={padding:'50px 20px' , width:600, margin:"20pxauto"}
-
-
+    const paperStyle={padding:'50px 20px' , width:600, margin:"20px auto"}
+    // const classes = useStyles();
+    const [name, setName] =useState('Ola')
+    const [nationality, setNationality] =useState('PL')
   return (
   <Container>
     <Paper elevation={3} style={paperStyle}>
+        <h1 style={{color:"dark"}}>Add Student</h1>
     <Box
       component="form"
       sx={{
         '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-
-    >
-      <TextField id="outlined-basic" label="Student name" variant="outlined" />
-      <TextField id="outlined-basic" label="Nationality" variant="outlined" />
-    </Box>
+      }}>
+        
+         {/* <form className={classes.root} noValidate autoComplete="off"> */}
+      <TextField id="outlined-basic" label="Student name" variant="outlined" fullWidth 
+      value={name}
+      onChange={(e)=>setName(e.target.value)}/>
+      <TextField id="outlined-basic" label="Nationality" variant="outlined" fullWidth
+      value={nationality}
+      onChange={(e)=>setNationality(e.target.value)}/>
+      {/* </form> */}
+      </Box>
     </Paper>
    </Container>
   );
